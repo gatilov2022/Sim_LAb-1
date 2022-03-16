7d33b793909c6ff4a7f59f11ff8bcd40f3b1bc51
norikov <norikov151@mail.ru>
2022-03-16 16:37:03 +0500

    6

--- Sim_LAb№1/Form1.cs
+++ Sim_LAb№1/Form1.cs
@@ -17,5 +17,34 @@ namespace Sim_LAb_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int expr = 0;
+            int orel = 0;
+
+            chart1.Series.Clear();
+
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                expr = 0;
+                orel = 0;
+                chart1.Series.Add(i.ToString());
+                chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[i].Color = Color.Black;
+
+                for (int j = 0; j < Convert.ToInt32(textBox1.Text); j++)
+                {
+                    double m = rand.NextDouble();
+                    expr++;
+                    if (m > 0.5)
+                    {
+                        orel++;
+                    }
+
+                    chart1.Series[i].Points.AddXY(expr, (double)orel / (double)expr);
+                }
+            }
+        }
     }
 }
