f0ab19af8d98938b42f4936827cc395df011fa77
Sneve <cerg.2002@bk.ru>
2022-03-21 13:19:25 +0500

    Захуярили рукава

--- Sim_LAb№1/Form1.Designer.cs
+++ Sim_LAb№1/Form1.Designer.cs
@@ -44,10 +44,11 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -57,57 +58,64 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 26);
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(240, 26);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.Size = new System.Drawing.Size(91, 29);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "1000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(133, 69);
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(240, 69);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.Size = new System.Drawing.Size(91, 29);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "100";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.Size = new System.Drawing.Size(195, 21);
             this.label1.TabIndex = 2;
             this.label1.Text = "Число опытов в серии:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(35, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.Size = new System.Drawing.Size(122, 21);
             this.label2.TabIndex = 3;
             this.label2.Text = "Кол-во серий:";
             // 
             // chart1
             // 
+            this.chart1.BackColor = System.Drawing.Color.LightGray;
             chartArea7.AxisX.Title = "Кол-во n в серии";
             chartArea7.AxisY.Interval = 0.5D;
             chartArea7.AxisY.Maximum = 1D;
             chartArea7.AxisY.Minimum = 0D;
             chartArea7.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea7);
-            this.chart1.Location = new System.Drawing.Point(248, 12);
+            this.chart1.Location = new System.Drawing.Point(381, 12);
             this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(421, 224);
+            this.chart1.Size = new System.Drawing.Size(696, 288);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 61);
+            this.button1.Location = new System.Drawing.Point(15, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 23);
+            this.button1.Size = new System.Drawing.Size(299, 53);
             this.button1.TabIndex = 5;
             this.button1.Text = "Применить";
             this.button1.UseVisualStyleBackColor = true;
@@ -116,25 +124,27 @@
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tabControl1.Location = new System.Drawing.Point(13, 118);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(229, 186);
+            this.tabControl1.Size = new System.Drawing.Size(331, 186);
             this.tabControl1.TabIndex = 6;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(221, 160);
+            this.tabPage1.Size = new System.Drawing.Size(323, 152);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Монетка";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // chart2
             // 
+            this.chart2.BackColor = System.Drawing.Color.LightGray;
             chartArea8.AxisY.Interval = 0.5D;
             chartArea8.AxisY.Maximum = 1D;
             chartArea8.AxisY.Minimum = 0D;
@@ -143,27 +153,30 @@
             legend4.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
             legend4.Name = "Legend1";
             this.chart2.Legends.Add(legend4);
-            this.chart2.Location = new System.Drawing.Point(248, 255);
+            this.chart2.Location = new System.Drawing.Point(381, 310);
             this.chart2.Name = "chart2";
             series10.BorderWidth = 2;
             series10.ChartArea = "ChartArea1";
             series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series10.Legend = "Legend1";
+            series10.LegendText = "Отклонение";
             series10.Name = "otkl";
             series11.BorderWidth = 2;
             series11.ChartArea = "ChartArea1";
             series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series11.Legend = "Legend1";
+            series11.LegendText = "По Лапласу";
             series11.Name = "laplas";
             series12.BorderWidth = 2;
             series12.ChartArea = "ChartArea1";
-            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series12.Legend = "Legend1";
+            series12.LegendText = "Практическая оценка";
             series12.Name = "prak";
             this.chart2.Series.Add(series10);
             this.chart2.Series.Add(series11);
             this.chart2.Series.Add(series12);
-            this.chart2.Size = new System.Drawing.Size(421, 245);
+            this.chart2.Size = new System.Drawing.Size(696, 300);
             this.chart2.TabIndex = 7;
             this.chart2.Text = "chart2";
             // 
@@ -171,51 +184,67 @@
             // 
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Location = new System.Drawing.Point(17, 310);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(9, 383);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(215, 163);
+            this.groupBox1.Size = new System.Drawing.Size(313, 163);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Результаты последних опытов:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 20);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Результат:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(161, 479);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(71, 20);
-            this.textBox3.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 482);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Вероятность отклонения:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(7, 51);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.Size = new System.Drawing.Size(0, 21);
             this.label5.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Результат:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(233, 567);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 29);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 570);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Вероятность отклонения:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(381, 631);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(228, 25);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Логарифмическая шкала";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 531);
+            this.ClientSize = new System.Drawing.Size(1089, 668);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.groupBox1);
@@ -255,6 +284,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Sim_LAb№1/Form1.cs
+++ Sim_LAb№1/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -17,6 +12,9 @@ namespace Sim_LAb_1
         public Form1()
         {
             InitializeComponent();
+            textBox3.Text = "0,96";
+
+            chart1.ChartAreas[0].BackColor = Color.LightGray;
         }
 
         private void create_graf(int col_ser,int col_popit)
@@ -24,24 +22,27 @@ namespace Sim_LAb_1
             chart1.Series.Clear();
             int expr;
             int orel;
-            for (int i = 0; i < col_ser; i++)
+            for (int i = 0; i < col_ser; i++) // Количество серий
             {
-                expr = orel = 0;
+                expr = orel = 0; 
                 chart1.Series.Add(i.ToString());
                 chart1.Series[i].ChartType = SeriesChartType.Line;
                 chart1.Series[i].Color = Color.Gray;
 
-                for (int j = 0; j < col_popit; j++)
+                for (int j = 0; j < col_popit; j++) // Количество опытов
                 {
-                    double m = rand.NextDouble();
-                    expr++;
-                    if (m >= 0.5)
+                    double m = rand.NextDouble(); // Число от 0 до 1 не включая
+                    expr++; // Кол-во эксперементов 
+                    if (m >= 0.5) // Проверка на выпадение орла
                     {
                         orel++;
                     }
                     chart1.Series[i].Points.AddXY(expr, (double)orel / (double)expr);
                 }
             }
+
+            
+            
             chart1.ChartAreas[0].AxisX.Title = "Номер эксперемента";
             chart1.ChartAreas[0].AxisY.Title = "Вероятность";
         }
@@ -53,26 +54,60 @@ namespace Sim_LAb_1
             chart1.Series["SredArif"].ChartType = SeriesChartType.Spline;
             chart1.Series["SredArif"].BorderWidth = 2;
 
+            chart1.Series.Add("Accuracy");
+            chart1.Series["Accuracy"].Color = Color.Gold;
+            chart1.Series["Accuracy"].ChartType = SeriesChartType.Line;
+            chart1.Series["Accuracy"].BorderWidth = 3;
+
+            chart1.Series.Add("Accuracy2");
+            chart1.Series["Accuracy2"].Color = Color.Gold;
+            chart1.Series["Accuracy2"].ChartType = SeriesChartType.Line;
+            chart1.Series["Accuracy2"].BorderWidth = 3;
             chart2.Series[0].Points.Clear();
+            chart2.Series[2].Points.Clear();
+            chart2.Series[1].Points.Clear();
+
+
+            double y;
+            List<double> NValues = new List<double>();
+
+            double Delta = (1 - Convert.ToDouble(textBox3.Text) )/ 2;
+
+            int AmountToDel = (int)(Delta * col_ser);
 
             for (int i = 0; i < col_popit; i++)
             {
-                double y = 0;
+                y = 0;
+                NValues.Clear();
                 for (int j = 0; j < col_ser; j++)
                 {
                     y += chart1.Series[j].Points[i].YValues[0];
+                    NValues.Add(chart1.Series[j].Points[i].YValues[0]);
                 }
+                NValues.Sort();
+                NValues.RemoveRange(NValues.Count - AmountToDel, AmountToDel);
+                NValues.RemoveRange(0, AmountToDel);
+
+                chart1.Series["Accuracy"].Points.AddXY(i + 1, NValues[0] + 0.01);
+                chart1.Series["Accuracy2"].Points.AddXY(i + 1, NValues[NValues.Count - 1] - 0.01);
+
+                chart2.Series[2].Points.AddXY(i + 1, (NValues[NValues.Count - 1] - NValues[0]) / 2);
+
                 double sred = y / (double)col_ser;
                 chart1.Series["SredArif"].Points.AddXY(i + 1, sred);
                 chart2.Series[0].Points.AddXY(i + 1, Math.Abs(sred - 0.5));
             }
+            
             chart2.ChartAreas[0].AxisX.Title = "Номер эксперемента";
             chart2.ChartAreas[0].AxisY.Title = "Погрешность";
 
         }
 
+
+
         private void rez_gtoup(int col_popit)
         {
+            
             double last_rez = chart1.Series["SredArif"].Points[col_popit - 1].YValues[0];
             if (last_rez.ToString().Length < 16)
             {
@@ -93,16 +128,32 @@ namespace Sim_LAb_1
             chart1.ChartAreas[0].AxisX.Maximum = col_popit + 1;
             chart2.ChartAreas[0].AxisX.Maximum = col_popit + 1;
 
-            chart1.ChartAreas[0].AxisX.IsLogarithmic = false;
-            chart2.ChartAreas[0].AxisX.IsLogarithmic = false;
-
+            if (checkBox1.Checked == true)
+            {
+                chart1.ChartAreas[0].AxisX.IsLogarithmic = true;
+                chart2.ChartAreas[0].AxisX.IsLogarithmic = true;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisX.IsLogarithmic = false;
+                chart2.ChartAreas[0].AxisX.IsLogarithmic = false;
+            }
             create_graf(col_ser, col_popit);
             create_graf_task2(col_ser,col_popit);
             rez_gtoup(col_popit);
 
 
-            chart1.ChartAreas[0].AxisX.IsLogarithmic = true;
-            chart2.ChartAreas[0].AxisX.IsLogarithmic = true;
+            
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 && textBox2.Text.Length > 0 && textBox3.Text.Length > 0 && Convert.ToDouble(textBox3.Text) < 1 && Convert.ToDouble(textBox3.Text) > 0)
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled = false;
+        }
+
     }
 }
